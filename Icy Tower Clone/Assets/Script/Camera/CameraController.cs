19dce482113391c065993f6e17cddb4487e20834
Hung <khiditdo1501@gmail.com>
2024-05-29 23:53:45 +0700

    implemented new go up when player reach top screen

--- Icy Tower Clone/Assets/Script/Camera/CameraController.cs
+++ Icy Tower Clone/Assets/Script/Camera/CameraController.cs
@@ -5,17 +5,46 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float cameraSpeed;
-    public float cameraIncreaseSpeed = 0.05f;
+    public float cameraIncreaseSpeed;
 
-    // Update is called once per frame
-    void Update()
+    // check if player on top screen
+    public Transform player; 
+    public float smoothSpeed = 0.125f;
+    public float verticalOffset = 2.0f; 
+    public float topScreenThreshold = 0.9f;
+
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void FixedUpdate()
     {
         if (GameManager.Instance.isPause)
             return;
 
-        transform.Translate(0, cameraSpeed * Time.deltaTime, 0);
+        if (GameManager.Instance.platformManager.playerHighestPlatform >= 5)
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), cameraSpeed * Time.deltaTime);
     }
 
+    private void LateUpdate()
+    {
+        CheckIfPlayerOnTopScreen();
+    }
+
+    private void CheckIfPlayerOnTopScreen()
+    {
+        Vector3 screenPoint = mainCamera.WorldToViewportPoint(player.position);
+        if (screenPoint.y >= topScreenThreshold)
+        {
+            Vector3 desiredPosition = new Vector3(transform.position.x, player.position.y + verticalOffset, transform.position.z);
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+        }
+    }
+  
     public void IncreaseScreenSpeed()
     {
         cameraSpeed += cameraIncreaseSpeed;
